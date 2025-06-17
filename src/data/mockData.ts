96da3bf1223ccdd566471a5f647724de4677d6eb
pedroka ϟ <pedrokaso123@gmail.com>
2025-06-17 19:54:54 +0000

    As contas disponiveis vão ser:
    Unranked Lvl 30+
    
    10 normal games jogado

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -1,18 +1,52 @@
+
 import type { Account } from '@/types';
 
 export const WHATSAPP_PHONE_NUMBER = '5500000000000'; // Replace with actual WhatsApp number e.g. 55119XXXXXXXX
 
 export const accountsData: Account[] = [
-  { id: 'acc001', name: 'Yasuo Imortal', price: 250, details: ['Skin Lendária Yasuo', 'Maestria 7', 'Elo: Diamante I'], isSold: false, image: 'https://placehold.co/300x200.png', imageHint: 'sword warrior' },
-  { id: 'acc002', name: 'Irelia Celestial', price: 220, details: ['Skin Divina Irelia', '25 Campeões', 'Elo: Platina IV'], isSold: false, image: 'https://placehold.co/300x200.png', imageHint: 'female warrior' },
-  { id: 'acc003', name: 'Sett Chefão', price: 180, details: ['Skin Sett Reinos Mech', 'Ícone Exclusivo', 'Elo: Ouro II'], isSold: true, image: 'https://placehold.co/300x200.png', imageHint: 'strong fighter' },
-  { id: 'acc004', name: 'Malphite Indestrutível', price: 150, details: ['Skin Shamrock Malphite', '15k Essência Azul', 'Elo: Prata III'], isSold: false, image: 'https://placehold.co/300x200.png', imageHint: 'rock monster' },
-  { id: 'acc005', name: 'Leona Solar Eclipse', price: 280, details: ['Skin Eclipse Solar Leona', 'Borda de Honra', 'Elo: Diamante III'], isSold: false, image: 'https://placehold.co/300x200.png', imageHint: 'sun knight' },
-  { id: 'acc006', name: 'Lux Elementalista PRO', price: 450, details: ['Skin Ultimate Lux', 'Todos Campeões Magos', 'Elo: Mestre'], isSold: false, image: 'https://placehold.co/300x200.png', imageHint: 'magic girl' },
-  { id: 'acc007', name: 'Ahri Florescer Espiritual', price: 200, details: ['Skin Florescer Ahri', 'Ícones KDA', 'Elo: Platina V'], isSold: false, image: 'https://placehold.co/300x200.png', imageHint: 'fox spirit' },
-  { id: 'acc008', name: 'Jinx Caos Intergalático', price: 260, details: ['Skin Lendária Jinx Odisseia', 'Maestria 7', 'Elo: Diamante IV'], isSold: false, image: 'https://placehold.co/300x200.png', imageHint: 'crazy shooter' },
-  { id: 'acc009', name: 'Kai\'Sa KDA ALL OUT', price: 300, details: ['Skin KDA Kai\'Sa Prestígio', 'Todas skins KDA', 'Elo: Mestre'], isSold: false, image: 'https://placehold.co/300x200.png', imageHint: 'pop star' },
-  { id: 'acc010', name: 'Zed Lâmina Sombria', price: 270, details: ['Skin Campeonato Zed', 'Full Chromas', 'Elo: Diamante II'], isSold: false, image: 'https://placehold.co/300x200.png', imageHint: 'ninja shadow' },
-  { id: 'acc011', name: 'Thresh Guardião Estelar', price: 190, details: ['Skin Guardião Estelar Thresh', 'Sentinelas Míticas', 'Elo: Ouro I'], isSold: false, image: 'https://placehold.co/300x200.png', imageHint: 'soul warden' },
-  { id: 'acc012', name: 'Soraka Emissária da Luz', price: 210, details: ['Skin Emissária da Luz Soraka', 'Ícones de Caridade', 'Elo: Platina II'], isSold: true, image: 'https://placehold.co/300x200.png', imageHint: 'star healer' },
+  {
+    id: 'unr001',
+    name: 'Conta Unranked Padrão #1',
+    price: 75,
+    details: ['Lvl 30+', '10+ normal games jogados', 'Pronta para Ranqueada'],
+    isSold: false,
+    image: 'https://placehold.co/300x200.png',
+    imageHint: 'league of legends unranked'
+  },
+  {
+    id: 'unr002',
+    name: 'Conta Unranked Padrão #2',
+    price: 75,
+    details: ['Lvl 30+', '10+ normal games jogados', 'Pronta para Ranqueada'],
+    isSold: false,
+    image: 'https://placehold.co/300x200.png',
+    imageHint: 'lol account'
+  },
+  {
+    id: 'unr003',
+    name: 'Conta Unranked Padrão #3',
+    price: 80, // Slight variation for example
+    details: ['Lvl 30+', '15+ normal games jogados', 'Pronta para Ranqueada'],
+    isSold: false,
+    image: 'https://placehold.co/300x200.png',
+    imageHint: 'new account'
+  },
+  {
+    id: 'unr004',
+    name: 'Conta Unranked Padrão #4',
+    price: 70,
+    details: ['Lvl 30+', '10+ normal games jogados', 'Pronta para Ranqueada'],
+    isSold: true, // Example of a sold account
+    image: 'https://placehold.co/300x200.png',
+    imageHint: 'gaming account'
+  },
+   {
+    id: 'unr005',
+    name: 'Conta Unranked Padrão #5',
+    price: 75,
+    details: ['Lvl 30+', '10+ normal games jogados', 'Pronta para Ranqueada'],
+    isSold: false,
+    image: 'https://placehold.co/300x200.png',
+    imageHint: 'unranked smurf'
+  },
 ];
